24bfae9464d4b21d88e6bad0b13c5995739627df
Owen Ouyang <oouyang@users.noreply.github.com>
2026-04-09 05:55:05 +0800

    Add Android, iOS, and GitHub Actions build setup
    - Android: WebView wrapper with geolocation, mipmap icons, Gradle build
    - iOS: SwiftUI WKWebView wrapper, Xcode project, 1024px app icon
    - CI: build-android.yml (debug+release APK), build-ios.yml (simulator+archive)
    - sync-web.sh scripts to copy web assets into native projects

---
+++ ios/Thsr/Thsr/ThsrApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct ThsrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WebViewScreen()
+                .ignoresSafeArea()
+        }
+    }
+}
